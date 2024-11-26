079a3bb01b4a02d987a80ad98c52d2f9ae4c3b08
thavlice <514531@mail.muni.cz>
2024-11-26 16:24:12 +0100

    Added display first line test

--- src/test/java/io/jenkins/plugins/LogFlowVisualizer/LogFlowFilterTest.java
+++ src/test/java/io/jenkins/plugins/LogFlowVisualizer/LogFlowFilterTest.java
@@ -187,5 +187,51 @@ class LogFlowFilterTest {
 
     }
 
+    @Test
+    void filter_withAdvancedConfigDisplayFirstLine_shouldSuccessfullyReturnLines() {
+        List<LineWithOffset> lines = new ArrayList<>();
+
+        lines.add(new LineWithOffset("START", 0));
+        lines.add(new LineWithOffset("SOME CONTENT 1", 1));
+        lines.add(new LineWithOffset("NOW", 2));
+        lines.add(new LineWithOffset("END", 3));
+
+        lines.add(new LineWithOffset("START", 4));
+        lines.add(new LineWithOffset("SOME CONTENT 2", 5));
+        lines.add(new LineWithOffset("YOU", 6));
+        lines.add(new LineWithOffset("END", 7));
+
+        lines.add(new LineWithOffset("START", 8));
+        lines.add(new LineWithOffset("SOME CONTENT 3", 9));
+        lines.add(new LineWithOffset("SEE", 10));
+        lines.add(new LineWithOffset("END", 11));
+
+        lines.add(new LineWithOffset("START", 12));
+        lines.add(new LineWithOffset("SOME CONTENT 4", 13));
+        lines.add(new LineWithOffset("ME", 14));
+        lines.add(new LineWithOffset("END", 15));
+
+        LogFlowInputAdvanced advancedConfig = new LogFlowInputAdvanced("START", "END", false, 30, 0);
+
+        List<LogFlowInput> configs = new ArrayList<>();
+        configs.add(advancedConfig);
+
+        List<LineOutput> result = LogFlowFilter.filter(lines, configs);
+
+        List<LineOutput> toDisplayResult = result.stream()
+                .filter(LineOutput::getDisplay)
+                .collect(Collectors.toList());
+
+        List<String> expectedLines = new ArrayList<>();
+        expectedLines.add("START");
+        expectedLines.add("START");
+        expectedLines.add("START");
+        expectedLines.add("START");
+        String lineSeparator = System.lineSeparator();
+        assertEquals(String.join(lineSeparator, expectedLines), toDisplayResult.stream()
+                .map(LineOutput::getLine)
+                .collect(Collectors.joining(lineSeparator)));
+    }
+
 
 }
